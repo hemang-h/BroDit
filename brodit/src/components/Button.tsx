8165f9e99ab3a2f8788d14fe72616e8fb8c90d7c
hemang-h <hemangcertificate@gmail.com>
2023-12-09 00:33:07 +0530

    ✅ Button Props

--- brodit/src/components/Button.tsx
+++ brodit/src/components/Button.tsx
@@ -5,3 +5,19 @@ export interface ButtonProps extends React.AllHTMLAttributes<HTMLButtonElement> 
     classes?: string;
 }
 
+export default function Button (props: ButtonProps) {
+    const{ classes, children, ...extraProps} = props;
+    return (
+        <button
+            {...extraProps}
+            className={
+                'bg-primary-400 px-[64px] py-[12px] text-white rounded-[20px] transition-colors ease-in-out duration-300 '
+                + 'hover:bg-primary hover:text-primary-400 disabled:bg-disabled disabled:text-fg-disabled'
+                + (classes ? ` ${classes}` : '')
+            }
+        >
+                {children}
+            </button>
+    );
+}
+
