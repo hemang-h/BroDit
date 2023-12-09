db47bcb7e2df02e823a04d808f2af15434960818
hemang-h <hemangcertificate@gmail.com>
2023-12-09 19:00:42 +0530

    ✨ Added Modular Arithmetic Operations

--- brodit/src/app/page.tsx
+++ brodit/src/app/page.tsx
@@ -11,7 +11,9 @@ import { useAccount, useDisconnect, useWalletClient } from 'wagmi'
 import UploadFile from '../components/UploadFile/UploadFile'
 import { Client } from '@web3-storage/w3up-client'
 import FilesList from '../components/FilesList/FilesList'
-import { formatAddress } from '@/utils/formatAddress'
+import { formatAddress } from '../../utils/formatAddress'
+import { create } from '@web3-storage/w3up-client'
+
 
 // Before starting run ETH Node with: npm run evm-node
 // Then deploy contract locally with: npm run deploy-contract
@@ -23,10 +25,13 @@ const CONTACT_ADDRESS = '0x73511669fd4dE447feD18BB79bAFeAC93aB7F31f'
 // CHAIN_ID: 31337
 // Import first account into Metamask from run eth-node output
 
+// cryptography
+import useModularArithmetic from './useModularArithmetic';
+import ModularArithmeticWorker from './modularArithmetic.worker;
+
 export default function Home() {
-    const [storageClient, setStorageClient] = useState<Client>();
     const [boxes, setBoxes] = useState<Brodit.BoxStructOutput[]>([])
-    // TODO: memos, state, etc...
+     // TODO: memos, state, etc...
     const { open } = useWeb3Modal();
     const { disconnect } = useDisconnect();
     const { address } = useAccount();
@@ -36,6 +41,51 @@ export default function Home() {
     //         client.createSpace('my-brodits')
     //     })
     // }, []);
+
+    const e = BigInt(3);
+    const m = BigInt(1000000007); // must be prime
+    const [forwardComputation, backwardComputation] = useModularArithmetic(e, m);
+    const [result, setResult] = useState<string>('');
+    const [progress, setProgress] = useState<number>(0);
+    const [worker, setWorker] = useState<Worker | null>(null);
+
+    useEffect(() => {
+        const newWorker = new ModularArithmeticWorker();
+        newWorker.onmessage = (e) => {
+            console.log("BLABLA");
+            if (e.data.progress) {
+                setProgress(e.data.progress);
+            }
+            if (e.data.result) {
+                setResult(`Result: ${e.data.result}`);
+            }
+        };
+        // newWorker.onerror = (error) => {
+        //     console.error('Error from worker:', error.message);
+        // };
+        setWorker(newWorker);
+
+        return () => {
+            newWorker.terminate();
+        };
+    }, []);
+
+    const handleComputation = () => {
+        console.log(worker);
+        worker?.postMessage({ 
+            x: '12345', 
+            e: '3', 
+            m: '1000000007', 
+            steps: '1000000',
+            type: 'forward' // or 'backward'
+        });
+    };
+
+    useEffect(() => {
+        create().then(client => {
+            client.createSpace('my-mementos')
+        })
+    }, []);
 
     const getContract = async () => {
         // TODO: Wallet Connect
@@ -85,6 +135,10 @@ export default function Home() {
     return (
         <WalletProvider>
             <main className="flex min-h-screen flex-col items-center p-24 gap-8">
+
+            <button onClick={handleComputation}>Run Computation</button>
+                <p>Progress: {progress}%</p>
+                <p>{result}</p>
 
                 {address ? (
                     <>
